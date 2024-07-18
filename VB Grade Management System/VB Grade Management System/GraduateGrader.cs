214333717c6f0a707d7127238a7b0a8d7b3b8cec
Keeghans <145812704+Keeghans@users.noreply.github.com>
2024-07-17 21:16:49 -0500

    Add files via upload

---
+++ VB Grade Management System/VB Grade Management System/GraduateGrader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework2
+{
+    public class GraduateGrader: Grader
+    {
+        private decimal stipend;
+
+        public decimal Stipend { get => stipend; set => stipend = value; }
+
+        public override decimal GetTotalPay()
+        {
+            return base.GetTotalPay() + this.Stipend;
+        }
+
+     }
+
+}
+
+
+
+
+
+
+
+
+
+    
